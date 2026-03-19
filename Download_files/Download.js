aedce7122d0c0cd7819c073fd1147a39a1150724
jserot <jocelyn.serot@uca.fr>
2026-03-19 17:39:59 +0100

    Fixed home-made buttons + urls

--- Download_files/Download.js
+++ Download_files/Download.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20260316
+// Created by iWeb 3.0.4 local-build-20260319
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
--- Where_files/Where.js
+++ Where_files/Where.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20260316
+// Created by iWeb 3.0.4 local-build-20260319
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
